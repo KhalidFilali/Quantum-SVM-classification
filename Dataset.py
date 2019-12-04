a590349efcd939b39ebf8f084cdae42f4ec0adf0
Khalid Filali <56809036+KhalidFilali@users.noreply.github.com>
2019-12-04 09:20:48 -0500

    Update Dataset.py

--- Dataset.py
+++ Dataset.py
@@ -1,1 +1,45 @@
+import numpy as np
+import scipy
+from scipy.linalg import expm
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.decomposition import PCA
 
+def Cell_image(training_size, test_size, n, PLOT_DATA):
+    class_labels = [r'A', r'B']
+    data, target = datasets.load_Cell_image(True)
+    sample_train, sample_test, label_train, label_test = train_test_split(data, target, test_size=0.3, random_state=12)
+
+    # Standardizing dataset, now what this means is we are simply standardizing the data in order to not get a bias.
+    std_scale = StandardScaler().fit(sample_train)
+    sample_train = std_scale.transform(sample_train)
+    sample_test = std_scale.transform(sample_test)
+
+    # Now reduce number of features to number of qubits
+    pca = PCA(n_components=n).fit(sample_train)
+    sample_train = pca.transform(sample_train)
+    sample_test = pca.transform(sample_test)
+
+    # Scale to the range (-1,+1)
+    samples = np.append(sample_train, sample_test, axis=0)
+    minmax_scale = MinMaxScaler((-1, 1)).fit(samples)
+    sample_train = minmax_scale.transform(sample_train)
+    sample_test = minmax_scale.transform(sample_test)
+
+    # Picking the training size of samples
+    training_input = {key: (sample_train[label_train == k, :])[:training_size] for k, key in enumerate(class_labels)}
+    test_input = {key: (sample_test[label_test == k, :])[:test_size] for k, key in enumerate(class_labels)}
+    
+#Plotting data
+    if PLOT_DATA:
+        for k in range(0, 2):
+            plt.scatter(sample_train[label_train == k, 0][:training_size],
+                        sample_train[label_train == k, 1][:training_size])
+#Plotting data
+        plt.title("Malaria Cell image dataset")
+        plt.show()
+
+    return sample_train, training_input, test_input, class_labels
